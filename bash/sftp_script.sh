3eb529d8dca09fa87ce9155e268b288d6772e7cd
wuyihang <13600296601@163.com>
2024-10-09 14:38:37 +0800

    Update sftp_script.sh: force to use PasswordAuthentication

--- bash/sftp_script.sh
+++ bash/sftp_script.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Install Dependencies
 # dnf install expect
 
+# Prepare temporary directory and file
 # dd if=/dev/zero of=1m.file bs=1M count=1
 # mkdir -p /tmp/sftp
 
 SFTP_HOST=127.0.0.1
-SFTP_PORT=2222
+SFTP_PORT=10022
 SFTP_USER=root
-SFTP_PASSWORD=${SFTP_PASSWORD:-qweiop}
+SFTP_PASSWORD=${SFTP_PASSWORD:-password}
 SFTP_PATH=/tmp/sftp
 SFTP_FILE=1m.file
 
@@ -16,7 +18,7 @@ COUNT=10
 
 for i in $(seq $COUNT); do
 /usr/bin/expect <<-EOF
-spawn sftp -oPort=$SFTP_PORT $SFTP_USER@$SFTP_HOST
+spawn sftp -o PreferredAuthentications=password -o PubKeyAuthentication=no -o PasswordAuthentication=yes -o Port=$SFTP_PORT $SFTP_USER@$SFTP_HOST
 expect {
     "*yes/no" { send "yes\n"; exp_continue }
     "*password:" { send "$SFTP_PASSWORD\n" }
